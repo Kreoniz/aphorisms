16d3d8686bcbc3704292eebdd721d72c98ccac5b
Kreoniz <kreoniz@mail.ru>
2024-02-11 15:10:34 +0600

    Add tailwind to the project

--- src/Aphorism.tsx
+++ src/Aphorism.tsx
@@ -1,30 +1,28 @@
 function Aphorism({ quote, author, tags }) {
   return (
-      <>
+      <div>
         <div>
-          <div>
-            <h3>Quote:</h3>
-            {quote}
-          </div>
-
-          <div>
-            <h3>Author:</h3>
-            {author}
-          </div>
-
-          <div>
-            <h3>Tags:</h3>
-            {tags
-            ? tags.map((tag, index) => {
-                return (
-                  <div key={index}>{tag}</div>
-                );
-            })
-            : null
-            }
-          </div>
+          <h3>Quote:</h3>
+          {quote}
         </div>
-      </>
+
+        <div>
+          <h3>Author:</h3>
+          {author}
+        </div>
+
+        <div>
+          <h3>Tags:</h3>
+          {tags
+          ? tags.map((tag, index) => {
+              return (
+                <div key={index}>{tag}</div>
+              );
+          })
+          : null
+          }
+        </div>
+      </div>
     );
 }
 
