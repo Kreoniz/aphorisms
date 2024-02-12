92ab34a9ff36487e6f1b7bf499d76162a8d03b7f
Kreoniz <kreoniz@mail.ru>
2024-02-12 21:09:22 +0600

    Add sidebar with tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { useState, useEffect } from 'react';
-import Aphorism from './Aphorism.tsx'
-
-const URL = 'http://api.quotable.io/';
-
-interface Item {
-  _id: string;
-  content: string;
-  author: string;
-  tags: Array<string>;
-}
+import Feed from './Feed.tsx'
+import Sidebar from './Sidebar.tsx'
 
 function App() {
-  const [aphorisms, setAphorisms] = useState([]);
-
-  async function getQuotes(amount=20, tags='famous-quotes') {
-    const quotes = await fetch(URL + 'quotes?tags=' + tags + '&limit=' + amount);
-    const json = await quotes.json();
-    return json.results;
-  }
-
-  useEffect(() => {
-    getQuotes()
-    .then((response) => {
-      setAphorisms(response);
-    });
-  }, []);
-
   return (
-    <div>
-      {aphorisms.map((item: Item) => {
-        return (
-          <div key={item._id} className="my-4 sm:mx-4 mx-2">
-            <Aphorism quote={item.content} author={item.author} tags={item.tags} />
-          </div>
-        )
-      })}
+    <div className="flex my-4">
+      <div>
+        <Feed />
+      </div>
+
+      <div className="min-w-min w-1/4 px-4">
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+
+const URL = 'http://api.quotable.io/';
+
+interface Tag {
+  _id: string;
+  name: string;
+  slug: string;
+}
+
+function Sidebar() {
+  const [tags, setTags] = useState([]);
+
+  async function getTags() {
+    const quotes = await fetch(URL + 'tags');
+    const json = await quotes.json();
+    return json;
+  }
+
+  useEffect(() => {
+    getTags()
+    .then((response) => {
+      setTags(response);
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className="font-bold text-2xl text-center">Tags</div>
+      <div>
+      {tags.map((tag: Tag) => {
+        return (
+          <div
+            className="block hover:underline hover:cursor-pointer w-max"
+            key={tag._id}
+            data-slug={tag.slug}
+          >
+            {tag.name}
+          </div>
+        );
+      })}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
