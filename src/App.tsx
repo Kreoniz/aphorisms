ef8f67202d284bc235c3257c15658feeb27f0767
Kreoniz <kreoniz@mail.ru>
2024-02-14 22:09:53 +0600

    Style the menu and add link to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Feed from './Feed.tsx'
 import Sidebar from './Sidebar.tsx'
+import { Link } from "react-router-dom";
 
 function App() {
   function toggleSidebar() {
@@ -11,13 +12,22 @@ function App() {
 
   return (
     <div className="h-auto relative">
-      <button
-        className="md:hidden hover:underline"
-        type="button"
-        onClick={toggleSidebar}
-      >
-        Toggle
-      </button>
+
+      <div className="flex justify-between items-center pt-2 px-3">
+        <div>
+          <Link to='/' className="text-xl sm:text-2xl font-bold hover:underline">Home</Link>
+        </div>
+
+        <button
+          className="md:hidden hover:underline"
+          type="button"
+          onClick={toggleSidebar}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-12 h-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
 
       <div className="flex my-4">
         <div>
@@ -27,7 +37,7 @@ function App() {
         <div
           id="sidebar"
           className="
-            px-4 hidden absolute left-0 bg-white w-full h-full p-4
+            px-4 hidden absolute left-0 bg-white w-full h-full
             md:min-w-min md:block md:static md:w-2/5
             xl:w-1/4
           "
