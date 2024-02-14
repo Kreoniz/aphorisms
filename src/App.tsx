36920c10c7e6461f7a56425e4d42e56b58a4f2bb
Kreoniz <kreoniz@mail.ru>
2024-02-14 21:51:03 +0600

    Add menu toggle for small screen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,38 @@ import Feed from './Feed.tsx'
 import Sidebar from './Sidebar.tsx'
 
 function App() {
-  return (
-    <div className="flex my-4">
-      <div>
-        <Feed />
-      </div>
+  function toggleSidebar() {
+    const sidebar: Element | null = document.querySelector('#sidebar');
+    if (sidebar) {
+      sidebar.classList.toggle('hidden');
+    }
+  }
 
-      <div className="min-w-min w-1/4 px-4">
-        <Sidebar />
+  return (
+    <div className="h-auto relative">
+      <button
+        className="md:hidden hover:underline"
+        type="button"
+        onClick={toggleSidebar}
+      >
+        Toggle
+      </button>
+
+      <div className="flex my-4">
+        <div>
+          <Feed />
+        </div>
+
+        <div
+          id="sidebar"
+          className="
+            px-4 hidden absolute left-0 bg-white w-full h-full p-4
+            md:min-w-min md:block md:static md:w-2/5
+            xl:w-1/4
+          "
+        >
+          <Sidebar />
+        </div>
       </div>
     </div>
   );
